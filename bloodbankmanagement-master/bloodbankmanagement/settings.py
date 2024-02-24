0fb8fe015fda1c9b9924d05b4a1d86faaa3d62e6
Atul14258 <112306423+Atul14258@users.noreply.github.com>
2024-02-24 22:00:06 +0530

    setting changes

--- bloodbankmanagement-master/bloodbankmanagement/settings.py
+++ bloodbankmanagement-master/bloodbankmanagement/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'TEMPLATE_DIR', 'static')
 LOGIN_REDIRECT_URL='/afterlogin'
 
 
