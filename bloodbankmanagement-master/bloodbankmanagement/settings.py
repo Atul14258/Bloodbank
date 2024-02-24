5ef4b13aa41e3b025a458b0f2a7a0415b7404a99
Atul14258 <112306423+Atul14258@users.noreply.github.com>
2024-02-24 21:06:28 +0530

    setting changes

--- bloodbankmanagement-master/bloodbankmanagement/settings.py
+++ bloodbankmanagement-master/bloodbankmanagement/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 LOGIN_REDIRECT_URL='/afterlogin'
 
